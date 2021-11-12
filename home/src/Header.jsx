eda72117bb50f0c4f96cd93a5c5c072d559a31cb
Yasir Karam <yasirkaram@gmail.com>
2021-11-12 21:20:34 +0300

    signed
    Signed-off-by: Yasir Karam <yasirkaram@gmail.com>

---
+++ home/src/Header.jsx
@@ -0,0 +1,8 @@
+import react from "react";
+export default function Header(){
+    return (
+    <div className="p-5 bg-blue-500 text-white text-3xl font-hold">
+        Fidget Sprinner World II
+    </div>
+    );
+}
